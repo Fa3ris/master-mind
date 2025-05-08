d0bb9b2564c991215c8f35be3dc70f1917f0dea9
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2025-05-08 22:53:47 +0200

    rng

---
+++ src/random.ts
@@ -0,0 +1,53 @@
+// hash function to generate the seed
+// https://stackoverflow.com/a/47593316
+function cyrb128(str: string) {
+  let h1 = 1_779_033_703;
+  let h2 = 3_144_134_277;
+  let h3 = 1_013_904_242;
+  let h4 = 2_773_480_762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597_399_067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2_869_860_233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951_274_213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2_716_044_179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597_399_067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2_869_860_233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951_274_213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2_716_044_179);
+  (h1 ^= h2 ^ h3 ^ h4), (h2 ^= h1), (h3 ^= h1), (h4 ^= h1);
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+// create random generator function, the seed is split in 4 numbers
+function sfc32(a: number, b: number, c: number, d: number) {
+  return () => {
+    a |= 0;
+    b |= 0;
+    c |= 0;
+    d |= 0;
+    const t = (((a + b) | 0) + d) | 0;
+    d = (d + 1) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    c = (c + t) | 0;
+    return (t >>> 0) / 4_294_967_296;
+  };
+}
+
+const seed = cyrb128(String(new Date().getTime()));
+const getRand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+
+/**
+ *
+ * @param {*} min inclusive
+ * @param {*} max inclusive
+ * @returns
+ */
+export function randInt(min: number, max: number) {
+  const minCeiled = Math.ceil(Math.min(min, max));
+  const maxFloored = Math.floor(Math.max(min, max));
+  return minCeiled + Math.floor(getRand() * (maxFloored - minCeiled + 1));
+}
